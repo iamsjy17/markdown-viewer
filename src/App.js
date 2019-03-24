563a22e8e127da4df5cc9de6c230491f6505805c
jewooSong <iamsjy0808@gmail.com>
2019-03-25 02:31:25 +0900

    Project Init by react-skeleton

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+// eslint-disable-next-line react/prefer-stateless-function
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <header>
+          <p>react-skeleton</p>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
